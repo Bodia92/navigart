e8ce1f0984901cc149bf0d7223e5df5adfd7c9f1
Богдан Понамарьов <97220953+Bodia92@users.noreply.github.com>
2024-11-04 19:07:14 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -91,6 +91,10 @@ document.addEventListener('DOMContentLoaded', function () {
         fadeEffect: { crossFade: true },
         virtualTranslate: true,
         effect: 'fade',
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
