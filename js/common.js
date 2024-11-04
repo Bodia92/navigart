4d45a16cd321dd7368bd1b5411e7c954e6efca12
Богдан Понамарьов <97220953+Bodia92@users.noreply.github.com>
2024-11-04 14:53:38 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,17 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const headerLinks = document.querySelectorAll('.header__link')
+    const offset = 0
+
+    headerLinks.forEach((link) => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault()
+
+            const targetId = link.getAttribute('data-id')
+            const targetSection = document.getElementById(targetId)
+
+            if (targetSection) {
+                const sectionPosition =
+                    targetSection.getBoundingClientRect().top +
+                    window.scrollY -
+                    offset
+
+                window.scrollTo({
+                    top: sectionPosition,
+                    behavior: 'smooth',
+                })
+            }
+        })
+    })
     const blockquoteText = document.getElementById('quoteText')
     if (blockquoteText) {
         const text = blockquoteText.textContent
         blockquoteText.innerHTML = ''
 
-        // Розбиття тексту на окремі літери
         text.split('').forEach((char) => {
             const span = document.createElement('span')
             span.textContent = char
             blockquoteText.appendChild(span)
         })
 
-        // Додавання класу active при скролі
         window.addEventListener('scroll', () => {
             const topOffset = blockquoteText.getBoundingClientRect().top
             const windowHeight = window.innerHeight
@@ -20,11 +41,39 @@ document.addEventListener('DOMContentLoaded', function () {
                 Array.from(blockquoteText.children).forEach((span, index) => {
                     setTimeout(() => {
                         span.classList.add('active')
-                    }, index * 30) // Можете налаштувати затримку
+                    }, index * 30)
                 })
             }
         })
     }
+
+    const arrowIcons = document.querySelectorAll('.arrow-icon')
+    let currentIndex = 0
+
+    function updateClasses() {
+        arrowIcons.forEach((icon) => {
+            icon.classList.remove('active', 'prev', 'next')
+        })
+
+        const prevIndex =
+            (currentIndex - 1 + arrowIcons.length) % arrowIcons.length
+        const nextIndex = (currentIndex + 1) % arrowIcons.length
+
+        arrowIcons[prevIndex].classList.add('prev')
+        arrowIcons[currentIndex].classList.add('active')
+        arrowIcons[nextIndex].classList.add('next')
+
+        currentIndex = nextIndex
+    }
+
+    setInterval(updateClasses, 400)
+
+    const menuTrigger = document.querySelector('.js-menu-trigger')
+    const body = document.body
+
+    menuTrigger.addEventListener('click', () => {
+        body.classList.toggle('menu-open')
+    })
 
     //sliders
 
@@ -51,29 +100,6 @@ document.addEventListener('DOMContentLoaded', function () {
     heroImages.controller.control = heroThumbs
     heroThumbs.controller.control = heroImages
 
-    // const sliderMade = new Swiper('[data-made-slider]', {
-    //     spaceBetween: 32,
-    //     speed: 800,
-    //     slidesPerView: 2,
-    //     observer: true,
-    //     observeParents: true,
-    //     watchSlidesVisibility: true,
-    //     watchSlidesProgress: true,
-    //     breakpoints: {
-    //         0: {
-    //             slidesPerView: 16,
-    //             spaceBetween: 1,
-    //         },
-    //         768: {
-    //             slidesPerView: 2,
-    //             spaceBetween: 32,
-    //         },
-    //     },
-    //     navigation: {
-    //         nextEl: '.swiper-button-next',
-    //         prevEl: '.swiper-button-prev',
-    //     },
-    // })
     document.querySelectorAll('[data-made-slider]').forEach((sliderElement) => {
         new Swiper(sliderElement, {
             spaceBetween: 32,
@@ -107,5 +133,6 @@ document.addEventListener('DOMContentLoaded', function () {
             },
         })
     })
+
     //sliders###
 })
